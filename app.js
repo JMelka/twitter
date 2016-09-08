d53dc836a68120b2fb08116ae098f975082d81d2
JMelka <JMelka@verizon.net>
2016-09-08 16:50:21 -0400

    End of Day 3

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+'use strict'
+
 var dbFile = require('./db');
 
 //Create database
-//dbFile.initDB();
+dbFile.initDB();
 
 //Insert User
-//dbFile.insertUser('John', 6);
+dbFile.insertUser('John', 'JMelka', 'ABC123', 'JMelka@Test.com');
+dbFile.insertUser('Haritha', 'Haritha_Profile', 'ABC123', 'Haritha@Test.com');
 
 //Insert Tweet
-//dbFile.insertTweet('Hello');
+dbFile.insertTweet('John', 'Hello World');
+dbFile.insertTweet('Haritha', 'Hello World 2');
+
+dbFile.updateUser('JOHN', 'JMelka@Test.com');
+
+var userId = 20;
 
 //get user id
-dbFile.getUserId('John');
+dbFile.getUserId('John', cbUserId);
+
+
+
+function cbUserId(err, result){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("TheUserId: " + result);
+        userId = result;
+    }
+
+}
--- db.js
+++ db.js
@@ -10,16 +10,24 @@ var db = new sqlite3.Database('Twitter.db');
 function initDB() {
     db.serialize(function () {
         //User table
-        db.run("CREATE TABLE User (Name TEXT, Follower_Cnt INT)");
+        db.run("CREATE TABLE User (Name TEXT, Profile TEXT, Password TEXT, LoginName TEXT)");
 
         //Tweet table
         db.run("CREATE TABLE Tweet (UserId INT, Msg TEXT, Insrt_TS DATETIME DEFAULT CURRENT_TIMESTAMP)");
 
+        //Following table
+        db.run("CREATE TABLE Following (Follower INT, Followee INT)");
+
+        //Like table
+        db.run("CREATE TABLE Like (UserId INT, TweetId INT)");
+
     });
 }
 
-function insertTweet(msg) {
-    db.each("SELECT rowid as Id FROM User where Name = 'John'", function (err, row) {
+function insertTweet(userName, msg) {
+    //db.each("SELECT rowid as Id FROM User where Name = 'John'", function (err, row) {
+    var stmt = "SELECT rowid as Id FROM User where Name = ?";
+    db.each(stmt, userName, function (err, row) {
         if (err) {
             console.log(err);
         } else {
@@ -43,15 +51,15 @@ function insertTweet(msg) {
 
 // }
 
-function insertUser(name, fcnt) {
-    var stmt = db.prepare("INSERT INTO User (Name, Follower_Cnt) VALUES (?, ?)");
-    stmt.run(name, fcnt);
+function insertUser(name, profile, password, loginName) {
+    var stmt = db.prepare("INSERT INTO User (Name, Profile, Password, LoginName) VALUES (?, ?, ?, ?)");
+    stmt.run(name, profile, password, loginName);
     stmt.finalize();
 }
 
 function getUser() {
     return new Promise(function (resolve, reject) {
-        db.all("SELECT rowid as Id, Name AS Name, Follower_Cnt AS Follower_Cnt FROM User", function (err, rows) {
+        db.all("SELECT rowid as Id, Name AS Name FROM User", function (err, rows) {
             if (err) {
                 console.log(err);
                 reject(err);
@@ -64,30 +72,45 @@ function getUser() {
 
 }
 
-function getUserId(userId) {
+function getUserId(userName, cb) {
     var stmt = "SELECT rowid as Id FROM User where Name = ?";
-    db.each(stmt, userId, function (err, row) {
+    db.each(stmt, userName, function (err, row) {
         if (err) {
             console.log(err);
+            return cb(err);
         } else {
             console.log("RowId = " + row.Id);
 
             var userId = row.Id;
             //var msg = 'Hello';
             console.log("UserId = " + userId);
-            return userId;
+            return cb(null, userId);
         }
     });
 }
 
 
-// function updateUser(db){
-
-// }
+function updateUser(name, loginName){
+    var stmt = db.prepare("Update User Set Name = ? where LoginName = ?");
+    stmt.run(name, loginName);
+    stmt.finalize();
+}
 
 // function deleteUser(db){
 
 // }
+
+function insertFollowing(follower, followee){
+    var stmt = db.prepare("INSERT INTO Following (Follower, Followee) VALUES (?, ?)");
+    stmt.run(follower, followee);
+    stmt.finalize();
+}
+
+function insertLike(userId, tweetId){
+    var stmt = db.prepare("INSERT INTO Like (UserId, TweetId) VALUES (?, ?)");
+    stmt.run(userId, tweetId);
+    stmt.finalize();
+}
 
 
 //db.close();
@@ -95,3 +118,4 @@ exports.initDB = initDB;
 exports.insertUser = insertUser;
 exports.insertTweet = insertTweet;
 exports.getUserId = getUserId;
+exports.updateUser = updateUser;
